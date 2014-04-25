ce4b51ea5898fec8ce46fed997ac65d2d6e6b440
Patrick Tombez <patrick.tombez@blues2.ch>
2014-04-25 13:43:21 +0200

    Update OpenERP version in conf file Update field definition in order to match changes in OE Fix Warning "Module is shadowed by another"

--- bss_phonenumbers/tests/test_phonenumbers.py
+++ bss_phonenumbers/tests/test_phonenumbers.py
@@ -3,22 +3,22 @@ from openerp.osv import osv
 import unittest2
 import openerp.tests.common as common
 
-from bss_phonenumbers import bss_phonumbers_fields
+from openerp.addons.bss_phonenumbers import bss_phonumbers_fields # @UnresolvedImport
 from phonenumbers import NumberParseException
- 
+
 class test_phonenumbers(common.SingleTransactionCase):
-     
+
     @classmethod
     def setUpClass(self):
-        super(test_phonenumbers,self).setUpClass()
-         
+        super(test_phonenumbers, self).setUpClass()
+
     @classmethod
     def tearDownClass(self):
-        super(test_phonenumbers,self).tearDownClass()
-     
+        super(test_phonenumbers, self).tearDownClass()
+
     def setUp(self):
         """ I create an array of phones. """
-        super(test_phonenumbers,self).setUp()
+        super(test_phonenumbers, self).setUp()
         self.pn = bss_phonumbers_fields.phonenumber()
         self.samples = [
             '9545551234,US',
@@ -27,9 +27,9 @@ class test_phonenumbers(common.SingleTransactionCase):
             '',
             '11111111111111111',
         ]
-         
+
     def tearDown(self):
-        super(test_phonenumbers,self).tearDown()
+        super(test_phonenumbers, self).tearDown()
 
     def test_00_phonenumbers_formatting_en_US(self):
         """ I check the phone is correctly formatted for en_US. """
@@ -38,7 +38,7 @@ class test_phonenumbers(common.SingleTransactionCase):
         self.assertEqual(res, '+19545551234', 'e164 phone formatting failed')
         res = self.pn._symbol_get(number_phone)
         self.assertEqual(res, '+1 954-555-1234', 'International phone formatting failed')
-        
+
     def test_10_phonenumbers_formatting_fr_CH(self):
         """ I check the phone is correctly formatted for fr_CH. """
         number_phone = self.samples[1]
@@ -46,13 +46,13 @@ class test_phonenumbers(common.SingleTransactionCase):
         self.assertEqual(res, '+41411234567', 'e164 phone formatting failed')
         res = self.pn._symbol_get(number_phone)
         self.assertEqual(res, '+41 41 123 45 67', 'International phone formatting failed')
-        
+
     def test_20_phonenumbers_UnicodeDecodeError(self):
         """ I check invalid characters are detected. """
         number_phone = self.samples[2]
         with self.assertRaises(osv.except_osv):
             self.pn._symbol_set_char(number_phone)
-            
+
     def test_30_phonenumbers_empty(self):
         """ I check the phone is correctly empty. """
         number_phone = self.samples[3]
@@ -60,12 +60,12 @@ class test_phonenumbers(common.SingleTransactionCase):
         self.assertEqual(res, None, 'e164 phone formatting failed')
         res = self.pn._symbol_get(number_phone)
         self.assertEqual(res, None, 'International phone formatting failed')
-        
+
     def test_40_phonenumbers_too_long(self):
         """ I check the stored phone is correctly empty. """
         number_phone = self.samples[4]
         with self.assertRaises(osv.except_osv):
             self.pn._symbol_set_char(number_phone)
-        
+
 if __name__ == '__main__':
     unittest2.main()
