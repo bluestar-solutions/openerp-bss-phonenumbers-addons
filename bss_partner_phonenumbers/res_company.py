03d6cff973a32ff661b720400e75e1cb82bce9f4
Hervé Martinet <herve.martinet@blues2.ch>
2016-11-01 08:56:44 +0100

    Fix company phone fields
    Functional fields cannot be phonenumbers, because this custom type is not managed in ORM class. This bug inject a field with no type is db stored schema (with no known effect). Switch to a char field with a phonenumber widget.

--- bss_partner_phonenumbers/res_company.py
+++ bss_partner_phonenumbers/res_company.py
@@ -29,11 +29,11 @@ class bss_partner_phonenumbers_company(osv.osv):
     _columns = {
         'phone': fields.function(res_company._get_address_data,
                                  fnct_inv=res_company._set_address_data,
-                                 size=64, type='phonenumber', string="Phone",
+                                 size=64, type='char', string="Phone",
                                  multi='address'),
         'fax': fields.function(res_company._get_address_data,
                                fnct_inv=res_company._set_address_data,
-                               size=64, type='phonenumber', string="Fax",
+                               size=64, type='char', string="Fax",
                                multi='address'),
     }
 
