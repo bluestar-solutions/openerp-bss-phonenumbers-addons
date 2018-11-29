a88fd1e341fec3bfb3e9e9a9717f82b2c6ceb623
Hervé Martinet <herve.martinet@bluestar.solutions>
2018-11-29 23:58:34 +0100

    Fix default country

--- bss_phonenumbers/models/res_company.py
+++ bss_phonenumbers/models/res_company.py
@@ -2,10 +2,15 @@
 # Part of Partner Phone Numbers.
 # See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields
+from odoo import api, models, fields
 
 
 class Company(models.Model):
     _inherit = 'res.company'
 
     country_id = fields.Many2one(required=True)
+
+    @api.model_cr
+    def init(self):
+        self.search([('partner_id.country_id', '=', False)]).mapped(
+            'partner_id').write({'country_id': self.env.ref('base.ch').id})
