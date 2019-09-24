b68f769b1fb761565ac4b31514de20075772d871
Hervé Martinet <herve.martinet@gmail.com>
2019-09-24 09:53:11 +0200

    Fix cascade delete and Odoo 10 guidelines

--- bss_partner_multi_phone/models/bss_partner_phone.py
+++ bss_partner_multi_phone/models/bss_partner_phone.py
@@ -2,7 +2,9 @@
 # Part of Partner Multiple Phone Numbers.
 # See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, api
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
 from odoo.addons.bss_phonenumbers import (
     fields as pnfields  # @UnresolvedImport
 )
@@ -12,28 +14,24 @@ class PartnerPhone(models.Model):
     _name = 'bss.partner.phone'
     _description = 'Partner Phone'
     _rec_name = 'number'
+    _order = "partner_id, sequence"
 
     number = pnfields.Phone("Number", required=True)
     category_id = fields.Many2one(
         'bss.phone.category', "Category", required=True)
-    partner_id = fields.Many2one('res.partner', "Partner", required=True)
-    sequence = fields.Integer('Sequence', help='Gives the sequence'
-                              'order when displaying a list of phone'
-                              'numbers.', default=10)
+    partner_id = fields.Many2one(
+        'res.partner', "Partner", required=True, ondelete='cascade')
+    sequence = fields.Integer(
+        'Sequence', help='Gives the sequence order when displaying '
+        'a list of phone numbers.', default=10)
 
-    _order = "partner_id, sequence"
-
-    @api.multi
+    @api.constrains('category_id')
     def _check_unique(self):
-        for phone in self:
-            if phone.category_id.unique:
-                if self.search([
-                    ('id', '!=', phone.id),
-                    ('partner_id', '=', phone.partner_id.id),
-                    ('category_id', '=', phone.category_id.id)
-                ], count=True):
-                    return False
-            return True
-
-    _constraints = [(_check_unique, 'A unique category cannot '
-                     'be used multiple times on a partner', ['category_id'])]
+        if self.category_id.unique and self.search([
+            ('id', '!=', self.id),
+            ('partner_id', '=', self.partner_id.id),
+            ('category_id', '=', self.category_id.id)
+        ], count=True):
+            raise ValidationError(_(
+                "A unique category cannot be used multiple times "
+                "on a partner."))
